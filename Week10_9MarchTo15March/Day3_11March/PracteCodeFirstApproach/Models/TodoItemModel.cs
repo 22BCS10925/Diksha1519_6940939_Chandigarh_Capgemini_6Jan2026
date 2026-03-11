c0860ff1c9184f0df7701d2f181047a8ef7101b1
Diksha <diksha11e@gmail.com>
2026-03-11 12:44:29 +0530

    Added Week10 projects

---
+++ Week10_9MarchTo15March/Day3_11March/PracteCodeFirstApproach/Models/TodoItemModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracteCodeFirstApproach.Models
+{
+    [Table("tblTodDo")]
+    public class TodoItemModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool isCompleted {  get; set; }
+
+    }
+}
