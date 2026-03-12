a1775969fdfabbc299024c99ddd90ce908e756f1
Diksha <diksha11e@gmail.com>
2026-03-12 15:35:01 +0530

    Initial Code

---
+++ Week10_9MarchTo15March/Day4_12March/LibraryManagement/Services/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using LibraryManagement.Models;
+namespace LibraryManagement.Services
+{
+    public class BookRepository : IBookRepository
+    {
+        private static List<Library> books = new List<Library>()
+    {
+        new Library{ Id = 1, Title = "C# Basics", Author="John"},
+        new Library{ Id = 2, Title = "ASP.NET Core", Author="Smith"},
+        new Library{ Id = 3, Title = "Entity Framework", Author="David"}
+    };
+
+        public IEnumerable<Library> GetAllBooks()
+        {
+            return books;
+        }
+
+        public Library GetBookById(int id)
+        {
+            return books.FirstOrDefault(b => b.Id == id);
+        }
+    }
+}
